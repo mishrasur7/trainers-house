bf052fd085ceb71f1b96ede569a3c1c82c14768c
Suraj Mishra <mishrasur7@gmail.com>
2022-04-27 22:31:23 +0300

    style to link tested

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
 import {
   BrowserRouter,
   Routes, 
@@ -9,6 +9,7 @@ import {
 import './App.css';
 import Customers from './components/customer/customers';
 import Trainings from './components/training/trainings';
+import { color } from '@mui/system';
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
       <BrowserRouter>
       <AppBar>
         <Toolbar>
-          <Link to="/customers">Customers</Link>
-          <Link to="/trainings">Trainings</Link>
+          <Link to="/customers" style={{textDecoration: 'none', margin: 'auto', color: 'white'}}>Customers</Link>
+          <Link to="/trainings" style={{textDecoration: 'none', margin: 'auto', color: 'white'}}>Trainings</Link>
         </Toolbar>
       </AppBar>
         <Routes>
@@ -25,7 +26,6 @@ function App() {
           <Route exact path='/trainings' element={<Trainings />} />
         </Routes>
       </BrowserRouter>
-    
     </div>
   );
 }
