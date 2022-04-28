e372ca1d911cf7424edcc8192061dd2d4e6de03d
Suraj Mishra <mishrasur7@gmail.com>
2022-04-28 22:40:58 +0300

    add customer added

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
       <Toolbar>
         <Link to='/' style={{textDecoration:'none', marginLeft: 150, color:'white', fontWeight:'bold', fontSize: 20}}>Customers</Link>
         <Link to='/trainings' style={{textDecoration:'none', marginLeft: 100, color:'white', fontWeight:'bold', fontSize: 20}}>Trainings</Link>
-        <Link to='/statistics' style={{textDecoration:'none', marginLeft: 100, color:'white', fontWeight:'bold', fontSize: 20}}>Statistics</Link>
         <Link to='/calendar' style={{textDecoration:'none', marginLeft: 100, color:'white', fontWeight:'bold', fontSize: 20}}>Calendar</Link>
+        <Link to='/statistics' style={{textDecoration:'none', marginLeft: 100, color:'white', fontWeight:'bold', fontSize: 20}}>Statistics</Link>
       </Toolbar>
     </AppBar>
       <Routes>
