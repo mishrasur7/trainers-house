bf063b19c5c65fe1f6fe405b0f1079704fe75646
Suraj Mishra <mishrasur7@gmail.com>
2022-05-02 13:47:46 +0300

    full calendar added

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react'
+import FullCalendar from '@fullcalendar/react' // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
 function Calendar () {
 
     return (
     <>
- 
+     <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
     </>
     ); 
 }
