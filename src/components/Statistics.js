ba6b858c5ab996fe4b4d393b4248e93250b97462
Suraj Mishra <mishrasur7@gmail.com>
2022-05-04 00:26:51 +0300

    rechart removed, chartjs-2 added, and statistics shown

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,26 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { BarChart, XAxis, YAxis, Bar } from 'recharts'; 
+//dependencies imported
+import {useEffect, useState} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+  } from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+
+//registering the plugins of ChartJS in order to implement chart 
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+  );
 
 function Statistics () {
+    //chartData object state to save our data from api
+    const [chartData, setChartData] = useState({
+        labels:[],
+        datasets: [
+          {
+            label: '',
+            data: [],
+            borderColor: '',
+            backgroundColor: ''
+          }
+        ]
+      });
 
-    const data = [
-        { name: 'Student1', value: 10 }, 
-        { name: 'Student2', value: 20 },
-        { name: 'Student2', value: 40 },
-        { name: 'Student2', value: 50 }, 
-    ]
+    //useEffect function to render data once the page is loaded
+    useEffect(() => { fetchData(); }, [])
+    
+    //fetching data from api, creating and storing required data into two arrays and finally 
+    //calling setChartData function to save our required data into chartData object state
+    const fetchData = () => {
+        const activities = []
+        const durations = [];
+        fetch('https://customerrest.herokuapp.com/gettrainings')
+        .then(response => {
+            if(response.ok) {
+                return response.json(); 
+            } else throw new Error (response.status)
+        })
+        .then(responseData => {
+            for (const data of responseData) {
+                activities.push(data.activity); 
+                durations.push(data.duration);
+            }
+            setChartData({
+                labels:activities,
+                datasets: [
+                  {
+                    label: 'Duration',
+                    data: durations,
+                    borderColor: 'white',
+                    backgroundColor: 'orange'
+                  }
+                ]
+              })
+        }
+        )
+        .catch(err => console.error(err))
+    }
 
-
-    return (
-        <>
-        <h1 style={{marginTop: 100}}>statistics test</h1>
-        <BarChart width={400} height={500} data={data}>
-            <XAxis dataKey='name' />
-            <YAxis dataKey='value' />
-            <Bar dataKey='value' fill='red'/>
-        </BarChart>
-        </>
-    )
+    //defining the axis, bar border width, responsiveness, position and title text
+    const options = {
+        indexAxis: 'x',
+        elements: {
+          bar: {
+            borderWidth: 1,
+          },
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          title: {
+            display: true,
+            text: 'Training activities and duration',
+          },
+        },
+      };
+    
+    return(
+        <div style={{width:'80%', height:'50%', margin: 100}}>
+            <Bar data={chartData} options={options}/>
+         </div>)
 }
 
-export default Statistics; 
+export default Statistics;
