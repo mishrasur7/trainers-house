3a59ba37b67e950b66b5f3d9f85ac6c648881749
Suraj Mishra <mishrasur7@gmail.com>
2022-05-03 12:56:59 +0300

    recharts installed and tested

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,8 +1,24 @@
+import React, { useState, useEffect } from "react";
+import { BarChart, XAxis, YAxis, Bar } from 'recharts'; 
+
 function Statistics () {
+
+    const data = [
+        { name: 'Student1', value: 10 }, 
+        { name: 'Student2', value: 20 },
+        { name: 'Student2', value: 40 },
+        { name: 'Student2', value: 50 }, 
+    ]
+
 
     return (
         <>
         <h1 style={{marginTop: 100}}>statistics test</h1>
+        <BarChart width={400} height={500} data={data}>
+            <XAxis dataKey='name' />
+            <YAxis dataKey='value' />
+            <Bar dataKey='value' fill='red'/>
+        </BarChart>
         </>
     )
 }
